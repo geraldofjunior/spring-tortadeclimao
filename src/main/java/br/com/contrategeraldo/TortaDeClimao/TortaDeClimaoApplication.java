3b35d6ed978c3d5753e71abd8f1a121fb28c1599
Geraldo <geraldofjunior@gmail.com>
2021-02-10 16:20:46 -0300

    Starting project

---
+++ src/main/java/br/com/contrategeraldo/TortaDeClimao/TortaDeClimaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.contrategeraldo.TortaDeClimao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortaDeClimaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TortaDeClimaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contrategeraldo/TortaDeClimao/TortaDeClimaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contrategeraldo.TortaDeClimao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortaDeClimaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
