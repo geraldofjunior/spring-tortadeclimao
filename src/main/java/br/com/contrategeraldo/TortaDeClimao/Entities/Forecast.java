423036a4e856ba8b9d4ace8cd2719175f3c393f4
Geraldo <geraldofjunior@gmail.com>
2021-02-11 22:42:45 -0300

    Versão 0.0.1

--- src/main/java/br/com/contrategeraldo/TortaDeClimao/Entities/Forecast.java
+++ src/main/java/br/com/contrategeraldo/TortaDeClimao/Entities/Forecast.java
@@ -1,88 +1,171 @@
 package br.com.contrategeraldo.TortaDeClimao.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
-import java.sql.Time;
 import java.util.Date;
 
 @Entity
 @Table(name = "forecast")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Forecast {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    private int townId;
-    private Date forecastDate;
+    private Date date;
+    private Date time;
+    private String condition_code;
+    private String description;
     private String weekday;
-    private int currentTemp;
-    private int maxTemp;
-    private int minTemp;
-    private String conditionDescription;
-    private String conditionSlug;
+    private int temp;
+    private int max;
+    private int min;
+    private String condition;
+    private String condition_slug;
+    private String slug;
     private String currently;
     private int humidity;
+    private String city_name;
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
-    public int getTownId() {
-        return townId;
+
+    public Date getDate() {
+        return date;
     }
-    public void setTownId(int townId) {
-        this.townId = townId;
+
+    public void setDate(Date date) {
+        this.date = date;
     }
-    public Date getForecastDate() {
-        return forecastDate;
+
+    public Date getTime() {
+        return time;
     }
-    public void setForecastDate(Date forecastDate) {
-        this.forecastDate = forecastDate;
+
+    public void setTime(Date time) {
+        this.time = time;
     }
+
+    public String getCondition_code() {
+        return condition_code;
+    }
+
+    public void setCondition_code(String condition_code) {
+        this.condition_code = condition_code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getWeekday() {
         return weekday;
     }
+
     public void setWeekday(String weekday) {
         this.weekday = weekday;
     }
-    public int getCurrentTemp() { return currentTemp; }
-    public void setCurrentTemp(int currentTemp) {
-        this.currentTemp = currentTemp;
+
+    public int getTemp() {
+        return temp;
     }
-    public int getMaxTemp() {
-        return maxTemp;
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
-    public void setMaxTemp(int maxTemp) {
-        this.maxTemp = maxTemp;
+
+    public int getMax() {
+        return max;
     }
-    public int getMinTemp() {
-        return minTemp;
+
+    public void setMax(int max) {
+        this.max = max;
     }
-    public void setMinTemp(int minTemp) {
-        this.minTemp = minTemp;
+
+    public int getMin() {
+        return min;
     }
-    public String getConditionDescription() {
-        return conditionDescription;
+
+    public void setMin(int min) {
+        this.min = min;
     }
-    public void setConditionDescription(String conditionDescription) {this.conditionDescription = conditionDescription;}
-    public String getConditionSlug() {
-        return conditionSlug;
+
+    public String getCondition() {
+        return condition;
     }
-    public void setConditionSlug(String conditionSlug) {
-        this.conditionSlug = conditionSlug;
+
+    public void setCondition(String condition) {
+        this.condition = condition;
     }
+
+    public String getCondition_slug() {
+        return condition_slug;
+    }
+
+    public void setCondition_slug(String condition_slug) {
+        this.condition_slug = condition_slug;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
     public String getCurrently() {
         return currently;
     }
+
     public void setCurrently(String currently) {
         this.currently = currently;
     }
+
     public int getHumidity() {
         return humidity;
     }
+
     public void setHumidity(int humidity) {
         this.humidity = humidity;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
+
+    @Override
+    public String toString() {
+        String jsonString = "Forecast{" +
+                "date: '" + this.date + "', " +
+                "time: '" + this.time + "', " +
+                "condition_code: '" + this.condition_code + "', " +
+                "description: '" + this.description + "', " +
+                "weekday: '" + this.weekday + "', " +
+                "temp: '" + this.temp + "', " +
+                "max: '" + this.max + "', " +
+                "min: '" + this.min + "', " +
+                "condition: '" + this.condition + "', " +
+                "condition_slug: '" + this.condition_slug + "', " +
+                "slug: '" + this.slug + "', " +
+                "currently: '" + this.currently + "', " +
+                "humidity: '" + this.humidity + "', " +
+                "city_name: '" + this.city_name + "', " + "}";
+        return jsonString;
     }
 }
--- src/main/java/br/com/contrategeraldo/TortaDeClimao/Interfaces/WeatherWebService.java
+++ src/main/java/br/com/contrategeraldo/TortaDeClimao/Interfaces/WeatherWebService.java
@@ -1,7 +1,24 @@
 package br.com.contrategeraldo.TortaDeClimao.Interfaces;
 
+import br.com.contrategeraldo.TortaDeClimao.Entities.Forecast;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherWebService {
+    private RestTemplateBuilder builder;
+    private RestTemplate template;
+
+    public WeatherWebService() {
+        this.builder = new RestTemplateBuilder();
+        this.template = this.builder.build();
+    }
+
+    @Bean
+    public Forecast getForecast(int woeid, RestTemplate template) throws Exception {
+        String url = "https://api.hgbrasil.com/weather?woeid=" + woeid;
+        return this.template.getForObject(url, Forecast.class);
+    }
 }
